226966e57db7e69115a677ce569b0d3de4c450d7
Ágatha almeida <agathasantos145@gmail.com>
2022-06-18 22:56:45 -0300

    Adicionar arquivos de projeto.

---
+++ CriarExcel-DataTable/Entradas/Entradas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriarExcel_DataTable.Entradas
+{
+    public class Entradas
+    {
+        string CaminhoPlanilha = @"c:\Dados\xlx\VendasEPPLus.xlsx";
+    }
+}
---
+++ CriarExcel-DataTable/Program.cs
@@ -0,0 +1,67 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Data;
+using System.IO;
+
+namespace CriarExcel_DataTable
+{
+    class Program
+    {
+        private static DataTable                     criar;
+        
+        static void Main(string[] args)
+        {
+            string caminhoPlanilha = @"C:\Nova pasta\Vendas.xlsx";
+            // define o nome do arquivo .xlsx 
+            Console.WriteLine("Criação de planilha");
+            CriarPlanilha(criar, caminhoPlanilha);
+        
+        }
+
+        private static void CriarPlanilha(DataTable criar, string caminhoPlanilha)
+        {
+            // define a licença
+            // cria instância do ExcelPackage 
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ExcelPackage excel = new ExcelPackage();
+
+            Console.WriteLine("Criando Planilha..");
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                Console.WriteLine("Criando DataTable");
+
+                DataTable dataTable = new DataTable("Planilha Excel");
+                dataTable.Columns.Add("Nomes", typeof(string));
+                dataTable.Columns.Add("Idades", typeof(int));
+                dataTable.Columns.Add("Endereço", typeof(string));
+
+                Console.WriteLine("Inserindo linhas");
+
+                dataTable.Rows.Add( "Macoratti", 21, "Meier");
+                dataTable.Rows.Add( "Jefferson", 20, "Rj");
+                dataTable.Rows.Add( "Janice", 20, "NI");
+                dataTable.Rows.Add( "Jessica", 25, "Engenho");
+                dataTable.Rows.Add( "Miriam", 48, "Nova Iguaçu");
+
+                //create a WorkSheet
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+
+                //add all the content from the DataTable, starting at cell A1
+                worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+                // Create excel file on physical disk 
+                FileStream objFileStrm = File.Create(caminhoPlanilha);
+                objFileStrm.Close();
+
+                Console.WriteLine("Escrevendo arquivo excel");
+
+                // Write content to excel file 
+                File.WriteAllBytes(caminhoPlanilha, excelPackage.GetAsByteArray());
+                excelPackage.SaveAs(new FileInfo(caminhoPlanilha));
+            }
+            
+            Console.WriteLine($"Planilha criada com sucesso");
+        }
+
+    }
+}
